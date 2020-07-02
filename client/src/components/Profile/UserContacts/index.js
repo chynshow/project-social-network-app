90f39534858e550e35f390c92e220e1491325d75
serhiichynshow <chynshow@protonmail.com>
2020-07-02 12:45:10 +0200

    refac styles of  components: Profile, User, Posts

--- client/src/components/Profile/UserContacts/index.js
+++ client/src/components/Profile/UserContacts/index.js
@@ -6,26 +6,28 @@ import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 const UserContacts = () => {
   return (
     <div className="c-user-contacts">
-      <div className="c-user-contacts__item">
-        <FontAwesomeIcon
-          className="c-user-contacts__item-icon"
-          icon={faEnvelope}
-        />
-        <span className="c-user-contacts__item-text">mail@mail.com</span>
-      </div>
-      <div className="c-user-contacts__item">
-        <FontAwesomeIcon
-          className="c-user-contacts__item-icon"
-          icon={faGithubSquare}
-        />
-        <span className="c-user-contacts__item-text">github.account</span>
-      </div>
-      <div className="c-user-contacts__item">
-        <FontAwesomeIcon
-          className="c-user-contacts__item-icon"
-          icon={faLinkedin}
-        />
-        <span className="c-user-contacts__item-text">linkedin</span>
+      <div className="c-user-contacts__items">
+        <div className="c-user-contacts__item">
+          <FontAwesomeIcon
+            className="c-user-contacts__item-icon"
+            icon={faEnvelope}
+          />
+          <span className="c-user-contacts__item-text">mail@mail.com</span>
+        </div>
+        <div className="c-user-contacts__item">
+          <FontAwesomeIcon
+            className="c-user-contacts__item-icon"
+            icon={faGithubSquare}
+          />
+          <span className="c-user-contacts__item-text">github.account</span>
+        </div>
+        <div className="c-user-contacts__item">
+          <FontAwesomeIcon
+            className="c-user-contacts__item-icon"
+            icon={faLinkedin}
+          />
+          <span className="c-user-contacts__item-text">linkedin</span>
+        </div>
       </div>
     </div>
   );
