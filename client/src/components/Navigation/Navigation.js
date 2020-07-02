d2809ce36698ee42262403534852209589081f4c
serhiichynshow <chynshow@protonmail.com>
2020-07-02 09:48:36 +0200

    del Button, Link & Icon components

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,49 +1,46 @@
 import React from 'react';
-import Icon from '../Common/Icon/Icon';
-import {
-  faUsers,
-  faUser,
-  faCogs,
-  // faCommentAlt,
-} from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers, faUser, faCogs } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = () => {
   return (
-    <ul className="c-main-navigation">
-      <NavLink
-        activeClassName="c-main-navigation__link--active"
-        className="c-link c-main-navigation__link"
-        to="/profile"
-      >
-        Profile
-        <Icon icon={faUser} />
-      </NavLink>
-      <NavLink
-        activeClassName="c-main-navigation__link--active"
-        className="c-link c-main-navigation__link"
-        to="/users"
-      >
-        Users
-        <Icon icon={faUsers} />
-      </NavLink>
-      {/* <NavLink
+    <div className="c-main-navigation">
+      <ul className="c-main-navigation__links-container">
+        <NavLink
+          activeClassName="c-main-navigation__link--active"
+          className="c-link c-main-navigation__link"
+          to="/profile"
+        >
+          Profile
+          <FontAwesomeIcon icon={faUser} />
+        </NavLink>
+        <NavLink
+          activeClassName="c-main-navigation__link--active"
+          className="c-link c-main-navigation__link"
+          to="/users"
+        >
+          Users
+          <FontAwesomeIcon icon={faUsers} />
+        </NavLink>
+        {/* <NavLink
         activeClassName="c-main-navigation__link--active"
         className="c-link c-main-navigation__link"
         to="/posts"
       >
         Messages
-        <Icon icon={faCommentAlt} />
+        <FontAwesomeIcon icon={faCommentAlt} />
       </NavLink> */}
-      <NavLink
-        activeClassName="c-main-navigation__link--active"
-        className="c-link c-main-navigation__link"
-        to="/settings"
-      >
-        Settings
-        <Icon icon={faCogs} />
-      </NavLink>
-    </ul>
+        <NavLink
+          activeClassName="c-main-navigation__link--active"
+          className="c-link c-main-navigation__link"
+          to="/settings"
+        >
+          Settings
+          <FontAwesomeIcon icon={faCogs} />
+        </NavLink>
+      </ul>
+    </div>
   );
 };
 
